c39226b80c07146218c0d485809259d2d399e957
Orin Chen <orinx@orinx.com>
2014-07-09 14:06:00 +0800

    add bootstrap calender

---
+++ events.json.php
@@ -0,0 +1,77 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "有人很缺德一次借了好幾天",
+			"url": "http://www.example.com/",
+			"class": "event-warning",
+			"start": "1362938400000",
+			"end":   "1363197686300"
+		},
+		{
+			"id": "256",
+			"title": "單日的活動",
+			"url": "http://www.example.com/",
+			"class": "event-warning",
+			"start": "1363155300000",
+			"end":   "1363227600000"
+		},
+		{
+			"id": "276",
+			"title": "測試活動測試活動測試活動測試活動測試活動測試活動",
+			"url": "http://www.example.com/",
+			"class": "event-success",
+			"start": "1363245600000",
+			"end":   "1363252200000"
+		},
+		{
+			"id": "294",
+			"title": "之後應該不會有這麼多顏色",
+			"url": "http://www.example.com/",
+			"class": "event-info",
+			"start": "1363111200000",
+			"end":   "1363284086400"
+		},
+		{
+			"id": "297",
+			"title": "綠色的活動",
+			"url": "http://www.example.com/",
+			"class": "event-success",
+			"start": "1363234500000",
+			"end":   "1363284062400"
+		},
+		{
+			"id": "54",
+			"title": "This is simple event",
+			"url": "http://www.example.com/",
+			"class": "",
+			"start": "1363712400000",
+			"end":   "1363716086400"
+		},
+		{
+			"id": "532",
+			"title": "重複",
+			"url": "http://www.example.com/",
+			"class": "event-inverse",
+			"start": "1364407200000",
+			"end":   "1364493686400"
+		},
+		{
+			"id": "548",
+			"title": "重複",
+			"url": "http://www.example.com/",
+			"class": "event-special",
+			"start": "1363197600000",
+			"end":   "1363629686400"
+		},
+		{
+			"id": "295",
+			"title": "測試",
+			"url": "http://www.example.com/",
+			"class": "event-important",
+			"start": "1364320800000",
+			"end":   "1364407286400"
+		}
+	]
+}
